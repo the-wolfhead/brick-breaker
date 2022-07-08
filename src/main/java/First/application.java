36ae6e38568f450c9d4fe891610fb9d12c5f6e4a
Daniel Otabor <danieldamianotabor@gmail.com>
2022-07-08 23:29:31 +0100

    Editing the main class

--- src/main/java/First/application.java
+++ src/main/java/First/application.java
@@ -4,13 +4,21 @@
  */
 package First;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author HP
  */
 public class application {
     public static void main(String[] args) {
-        
+        JFrame obj = new JFrame();
+        //GamePlay gamePlay = new GamePlay();
+        obj.setBounds(10, 10, 700, 600);
+        obj.setTitle("Brick Breaker");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
 }
